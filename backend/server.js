41a6e634a1e9493ba0ddae40aa53928dfedd27b6
Priyanshu Gupta <priyanshugupta1506@gmail.com>
2026-03-19 12:09:55 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://bbms-frontend.vercel.app", "*"],
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true,
 }));
 
